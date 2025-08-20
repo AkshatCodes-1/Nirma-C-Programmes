626e90a9fc4b17cc1a00ef21dc87490e5cd4a79c
AkshatCodes-1 <aks07akshat@gmail.com>
2025-08-21 02:04:50 +0530

    Add files via upload

---
+++ problem1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a ;
+    int b;
+    printf("enter your 1st number:\n",a);
+    scanf("%d",&a);
+    printf("enter your 2nd number:\n",b);
+    scanf("%d",&b);
+    printf("a when added with b gives: %d \n", a+b);
+    printf("a when subtracted with b gives:%d \n", a-b);
+    printf("a when multiplied with b gives:%d \n", a*b);
+    printf("a when divided with b gives:%d \n", a/b);
+    printf("a modulous b gives:%d \n", a%b);
+
+
+    return 0;
+}
